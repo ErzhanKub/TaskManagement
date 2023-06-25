8511978814606279d5eabba10979888e2e225688
Erzhan Kubanchbek Uulu <avazov.erjan@gmail.com>
2023-06-25 11:45:22 +0600

    Добавил классы и расширил функциональность
    Добавил ряд классов и папок
    Классы содержат базовый функционал

---
+++ Task_Meneger/Task_Meneger/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Meneger.Models
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        private string _firstname;
+        public string FirstName
+        {
+            get => _firstname;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Имя не можеть быть пустым", nameof(value));
+                }
+                _firstname = value;
+            }
+        }
+
+    }
+}
